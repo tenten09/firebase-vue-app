67d0bd17c3b20f01e57f660d97dc7973cfe563c9
tenten09 <y.koh3103@gmail.com>
2021-04-13 21:00:32 +0900

    todo app

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-container>
+      <b-input-group>
+        <b-form-input type="text" v-model="value"/>
+        <!-- <b-form-input type="text" v-model="value" key="type" /> -->
+
+        <!-- <b-row class="my-1" v-for="type in types" :key="type">
+          <b-col sm="3">
+            <label :for="`type-${type}`">Type <code>{{ type }}</code>:</label>
+          </b-col>
+
+          <b-col sm="9">
+            <b-form-input :id="`type-${type}`" :type="type"/>
+          </b-col>
+        </b-row> -->
+        <b-input-group-append>
+          <b-button variant="info" @click="handleAddTodo">送信</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoForm',
+    data() {
+      return {
+        value: '',
+        types: [
+          'date',
+          'time',
+        ],
+      };
+    },
+    methods: {
+      handleAddTodo() {
+        this.$emit('handleParentAddTodo', this.value);
+        this.value = "";
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-container>
+      <b-card
+        title="TODO"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="height: 20;"
+        class="mb-2" 
+        img-height="240"
+     >
+        <v-list two-line>
+
+            <b-row :key="index" v-for="(todo, index) in todos.slice(0, 6)" class="mt-2">
+              <b-col cols="8" :class="[{line: todo.complete}, 'text-left']">
+                <h5>{{todo.text}}</h5>
+                <h5>{{todo.type}}</h5>
+              </b-col>
+              <b-col cols="4" class="text-right">
+                <b-button 
+                  @click="handleCompleteTodo(index)" 
+                  :variant="todo.complete ? '' : 'primary'">
+                  {{todo.complete ? '完了' : '未完了'}}
+                </b-button>
+                <b-button @click="handleDeleteTodo(index)" variant="danger">delete</b-button>
+              </b-col>
+            </b-row>
+        </v-list>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'TodoList',
+    props: ['todos'],
+    methods: {
+      handleDeleteTodo(index) {
+        this.$emit("handleParentDeleteTodo", index);
+      },
+      handleCompleteTodo(index) {
+        this.$emit('handleParentCompleteTodo', index);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .line {
+    text-decoration: line-through;
+  }
+</style>
